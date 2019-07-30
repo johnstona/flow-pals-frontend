00a17c403a319d68a68db62728ce23b1472b8fa4
Alex Johnston <alexjohnston@ymail.com>
2019-07-30 20:59:48 +0100

    project updating

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Project.css'
 
 const Project = ({ project, collaborators, saveProject }) => {
@@ -7,12 +7,13 @@ const Project = ({ project, collaborators, saveProject }) => {
   const [name, setName] = useState(project.name)
 
   const handleHeaderChange = e => {
-    saveProject(project.id, e.target.value, content)
+    setName(e.target.value)
   }
+
+  useEffect(() => {saveProject(project.id, name, content)}, [content, name, project.id, saveProject])
 
   const handleProjectChange = e => {
     setContent(e.target.innerHTML)
-    saveProject(project.id, name, e.target.innerHTML);
   }
 
 
@@ -21,10 +22,10 @@ const Project = ({ project, collaborators, saveProject }) => {
       <textarea class="h1"
         data-id="h1"
         onChange={handleHeaderChange}
-        value={ project.name }
+        value={ name }
       />
       <div class="project" data-id="project" onChange={handleProjectChange}>
-        { project.content }
+        { content }
       </div>
     </div>
   )
